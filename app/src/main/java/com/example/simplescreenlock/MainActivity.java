02b21ba8b317e63d3ba624ff3ceb01b9d47f6074
Hiroshi_Nagata <oaoaarashi7@yahoo.co.jp>
2020-04-06 23:42:28 +0900

    feat:セッターとゲッターを実装、別Activityの実装

--- app/src/main/java/com/example/simplescreenlock/MainActivity.java
+++ app/src/main/java/com/example/simplescreenlock/MainActivity.java
@@ -1,19 +1,23 @@
 package com.example.simplescreenlock;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CountDown mCountDown;
+    private CountDown mSecondsCountDown;
+    private CountDown mMinutesCountDown;
     private TextView mTextTimer;
     private SimpleDateFormat mSimpleDateFormat = new SimpleDateFormat("HH:mm:ss", Locale.JAPAN);
     private Button mButtonStart;
@@ -22,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     private NumberPicker mMinutesNumberPicker;
     private NumberPicker mHourNumberPicker;
     private long mSeconds;
+    private long mMinutes;
+    private long mHour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,36 +40,76 @@ public class MainActivity extends AppCompatActivity {
 
         initNumberPicker();
 
+        mHourNumberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                setHour(mHourNumberPicker.getValue());
+            }
+        });
+
+        mMinutesNumberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                setMinutes(mMinutesNumberPicker.getValue());
+            }
+        });
+
         mSecondsNumberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker numberPicker, int i, int i1) {
-                mSeconds = numberPicker.getValue() * 1000;
-                mCountDown = new CountDown(mSeconds, 1000);
+                setSeconds(mSecondsNumberPicker.getValue());
+            }
+        });
 
-                mTextTimer = findViewById(R.id.text_timer);
-                mTextTimer.setText(mSimpleDateFormat.format(mInitialValue + mSeconds));
+//        mSecondsNumberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+//            @Override
+//            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+//                mSeconds = numberPicker.getValue() * 1000;
+//                mSecondsCountDown = new CountDown(mSeconds, 1000);
+//
+//                mTextTimer = findViewById(R.id.text_timer);
+//                mTextTimer.setText(mSimpleDateFormat.format(mInitialValue + getMinutes()));
+//
+//                mSecondsCountDown.setOnFinishListener(new OnFinishListener() {
+//                    @Override
+//                    public void onFinish() {
+//                        mTextTimer.setText(mSimpleDateFormat.format(mInitialValue));
+//                    }
+//                });
+//
+//                mSecondsCountDown.setOnTickListener(new OnTickListener() {
+//                    @Override
+//                    public void onTick(long millisUntilFinished) {
+//                        mTextTimer.setText(mSimpleDateFormat.format(millisUntilFinished + mInitialValue));
+//                    }
+//                });
+//
+//                mButtonStart = findViewById(R.id.button_timer_start);
+//                mButtonStart.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        mSecondsCountDown.start();
+//                    }
+//                });
+//            }
+//        });
 
-                mCountDown.setOnFinishListener(new OnFinishListener() {
-                    @Override
-                    public void onFinish() {
-                        mTextTimer.setText(mSimpleDateFormat.format(mInitialValue));
-                    }
-                });
-
-                mCountDown.setOnTickListener(new OnTickListener() {
-                    @Override
-                    public void onTick(long millisUntilFinished) {
-                        mTextTimer.setText(mSimpleDateFormat.format(millisUntilFinished + mInitialValue));
-                    }
-                });
-
-                mButtonStart = findViewById(R.id.button_timer_start);
-                mButtonStart.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        mCountDown.start();
-                    }
-                });
+        mButtonStart = findViewById(R.id.button_timer_start);
+        mButtonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle(R.string.text_title_dialog_lock_start)
+                        .setMessage(R.string.text_message_dialog_lock_start)
+                        .setPositiveButton(R.string.text_positive_dialog_lock_start, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Intent intent = new Intent(getApplicationContext(), ScreenLockActivity.class);
+                                startActivity(intent);
+                            }
+                        })
+                        .setNegativeButton(R.string.text_negative_dialog_lock_start, null)
+                        .show();
             }
         });
     }
@@ -80,6 +126,30 @@ public class MainActivity extends AppCompatActivity {
         mHourNumberPicker.setValue(0);
         mHourNumberPicker.setMaxValue(23);
         mHourNumberPicker.setMinValue(0);
+    }
+
+    public long getSeconds() {
+        return mSeconds;
+    }
+
+    public void setSeconds(long seconds) {
+        this.mSeconds = seconds;
+    }
+
+    public long getMinutes() {
+        return mMinutes;
+    }
+
+    public void setMinutes(long minutes) {
+        this.mMinutes = minutes;
+    }
+
+    public long getHour() {
+        return mHour;
+    }
+
+    public void setHour(long hour) {
+        this.mHour = hour;
     }
 }
 
---
+++ app/src/main/java/com/example/simplescreenlock/ScreenLockActivity.java
@@ -0,0 +1,14 @@
+package com.example.simplescreenlock;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScreenLockActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_screen_lock);
+    }
+}
